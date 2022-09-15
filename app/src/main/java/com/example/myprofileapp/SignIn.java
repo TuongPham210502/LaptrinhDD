9c1bc2f07683a1d7b328ab668637e6c231a5c4f8
= <=>
2022-09-15 11:45:12 +0700

    event sign in

--- app/src/main/java/com/example/myprofileapp/SignIn.java
+++ app/src/main/java/com/example/myprofileapp/SignIn.java
@@ -2,13 +2,49 @@ package com.example.myprofileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignIn extends AppCompatActivity {
-
+    private Button bt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        EditText username= (EditText) findViewById(R.id.username);
+        EditText password= (EditText) findViewById(R.id.password);
+
+
+        bt = (Button) findViewById(R.id.bt_signin);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("tuong123")&&password.getText().toString().equals("12345")){
+                    openProfile();
+                }
+                else{
+                    Toast.makeText(SignIn.this,"Login failed",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+    }
+    public void openProfile(){
+        Intent intent = new Intent(this, Profile.class);
+        startActivity(intent);
     }
 }
