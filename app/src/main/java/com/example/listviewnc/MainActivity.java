4efc7415a6fb7538098296f79eba4d9dffc105ec
= <=>
2022-09-30 08:59:50 +0700

    add va xoa

--- app/src/main/java/com/example/listviewnc/MainActivity.java
+++ app/src/main/java/com/example/listviewnc/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.listviewnc;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
     ListView lvPhim;
@@ -34,8 +40,20 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String tenPhim = edtTenPhim.getText().toString();
                 String namSX = edtNamsx.getText().toString();
-                arrPhim.add(new Phim(R.drawable.img_1,tenPhim,namSX));
+                arrPhim.add(new Phim(R.drawable.img_3,tenPhim,namSX));
+                Toast.makeText(MainActivity.this,"Thêm thành công",Toast.LENGTH_SHORT).show();
                 adapter.notifyDataSetChanged();
+            }
+        });
+
+        lvPhim.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                arrPhim.remove(i);
+                adapter.notifyDataSetChanged();
+                Toast.makeText(MainActivity.this,"Xóa thành công",Toast.LENGTH_SHORT).show();
+
+                return false;
             }
         });
     }
@@ -44,8 +62,8 @@ public class MainActivity extends AppCompatActivity {
         lvPhim = (ListView) findViewById(R.id.listviewPhim);
         arrPhim = new ArrayList<>();
 
-        arrPhim.add(new Phim(R.drawable.img_2,"Co dau 8 tuoi","2018"));
-        arrPhim.add(new Phim(R.drawable.img_1,"Co dau 8 tuoi","2018"));
-        arrPhim.add(new Phim(R.drawable.img_3,"Co dau 8 tuoi","2018"));
+        arrPhim.add(new Phim(R.drawable.img,"Aquaman","2020"));
+        arrPhim.add(new Phim(R.drawable.img_1,"Avengers: Endgame","2018"));
+        arrPhim.add(new Phim(R.drawable.img_2,"Tom and Jerry","2008"));
     }
 }
